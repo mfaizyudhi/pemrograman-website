8e92211aff81a5734bc85f77119472a11a69ccb2
mfaizyudhi <mfaiz091004@gmail.com>
2025-03-25 07:56:39 +0700

    [Pertemuan 3] Membuat Layout dan Component

--- resources/views/web/homepage.blade.php
+++ resources/views/web/homepage.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <h3>Ini adalah halaman Homepage</h3>
+    <div class="container mt-5">
+        <h3>Ini adalah halaman Homepage</h3>
+
+        {{-- Panggil Komponen Card --}}
+        <x-card-component />
+
+        <br>
+
+        {{-- Panggil Komponen Alert --}}
+        <x-alert-component />
+    </div>
 </x-layout>
